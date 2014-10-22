dffe5e4979b2c53c1d4ee5e62bb085c6c190c58e
sebastian <seba@nodomain>
2014-10-22 00:53:20 -0300

    Ejercicio 2.1 listo para presentar

--- Ejercicio2.m
+++ Ejercicio2.m
@@ -9,6 +9,32 @@
 % * asdasdfa
 clc; close all; clear all;
 
+x1 = [-3 -1.5 0.5 -1.5 -3 ];
+y1 = [-3 -3   0  3    3 ];
+
+x2 = [-1.5  3  3 0.5];
+y2 = [-3   -3  0 0];
+
+x3 = [-1.5 0.5 3 3 ];
+y3 = [3 0 0 3 ];
+
+hold on
+fill(x1,y1,.9 *[1 1 1])
+fill(x2,y2,.8 *[1 1 1])
+fill(x3,y3,.7 *[1 1 1])
+
+plot([2 2 -1 -1], [1 -1 1 -1], '*k')
+text([2 2 -1 -1], [0.8 -1.2 0.8 -1.2],{'p_1','p_2','p_3','p_4'})
+text([-2 1 1], [0 2 -2],{'C_1','C_2','C_3'})
+
+hold off
+axis square
+xlabel('x_1')
+ylabel('x_2')
+% legend('C_1', 'C_2', 'C_3', 'Location','EastOutside')
+
+%%
+
 % Genero la grilla
 % N = 100
 % x1 = linspace(-2,2,N);
