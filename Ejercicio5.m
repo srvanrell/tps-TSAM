87215c950c274d6e2950787226018c2781b83ae9
sebastian <seba@nodomain>
2014-11-06 00:14:25 -0300

    Ejercicio5, perceptron simple funcionando

--- Ejercicio5.m
+++ Ejercicio5.m
@@ -4,7 +4,7 @@ clc; clear all; close all;
 %% 1
 % Genere un conjunto de datos aleatorios bidimensionales...
 
-N = 400; % cantidad de muestras
+N = 200; % cantidad de muestras
 
 mu1 = [1 1.5];
 sigma1 = [0.5 0.3;
@@ -14,15 +14,57 @@ mu2 = [4 4];
 sigma2 = [1.2 -0.7;
           -0.7 3];
 
-x1 = mvnrnd(mu1, sigma1, N);
-x2 = mvnrnd(mu2, sigma2, N);
+x1 = mvnrnd(mu1, sigma1, N); % ejemplos de la clase 1 (+)
+x2 = mvnrnd(mu2, sigma2, N); % ejemplos de la clase 2 (o)
+eti1 = ones(N,1);            % etiquetas de los ejemplos de la clase 1 (+)
+eti2 = (-1)*ones(N,1);       % etiquetas de los ejemplos de la clase 2 (o)
+
 
 hold on
 plot(x1(:,1), x1(:,2),'+b')
 plot(x2(:,1), x2(:,2),'or')
+legend('clase 1','clase 2')
+title('Distribuciones originales')
+axis equal
 hold off
 
 
+% Separacion entre entrenamiento y testeo, para cuando corresponda
+ntrain = floor(0.7 * N); % 70% de los ejemplos son entrenamiento
+                         % 30% para testeo
+
+xtrain = vertcat(x1(1:ntrain,:), x2(1:ntrain,:)); % ejemplos de entrenamiento
+etitrain = vertcat(eti1(1:ntrain), eti2(1:ntrain));% etiquetas para entrenamiento supervisado
+xtest = vertcat(x1(ntrain+1:end,:), x2(ntrain+1:end,:)); % ejemplos de testeo
+etitest = vertcat(eti1(ntrain+1:end), eti2(ntrain+1:end));% etiquetas para testeo
+
+% Ordeno de forma aleatoria los ejemplos de entrenamiento
+ordentrain = randperm(2*ntrain);
+xtrain = xtrain(ordentrain,:);
+etitrain = etitrain(ordentrain);
+
 %% 2
 % ImplementaciÃ³n perceptrÃ³n simple
+aprendizaje = 0.001;
+toler = 0.01;
+[etipercep, w] = perceptron(xtrain,etitrain,aprendizaje,toler);
+
+x1train = xtrain(etipercep == eti1(1),:);
+x2train = xtrain(etipercep == eti2(1),:);
+
+figure
+hold on
+plot(x1train(:,1), x1train(:,2),'+b')
+plot(x2train(:,1), x2train(:,2),'or')
+plot(xtrain(etipercep~=etitrain,1), xtrain(etipercep~=etitrain,2),'.k')
+plot([1 4],-(w(1)+w(2)*[1 4])./w(3),'k-')
+legend('clase 1','clase 2','errores')
+title('Clasificación con Perceptrón Simple')
+axis equal
+hold off
+
+
+
+%% 3
+% Implementar k-medias
 
--- perceptron.m
+++ perceptron.m
@@ -1,18 +1,43 @@
-function w = perceptron( X, d, alfa,  )
+function [y, w] = perceptron( X, yd, alfa, tolerancia, iterMax)
 %UNTITLED6 Summary of this function goes here
 %   X son las caracteristicas, cada ejemplo en un renglon
-%   d es la salida deseada, la clase de cada ejemplo
-%   alfa el coeficiente de entrenamiento
-
-alfa = 0.05;
+%   yd es la salida deseada, la clase de cada ejemplo
+%   alfa el coeficiente de aprendizaje
+%   tolerancia de error admitida durante el entrenamiento
+%   w es el vector de pesos aprendidos, el primer elemento es del bias, w0
+if nargin < 3
+    alfa = 0.05; % coef de aprendizaje
+end
+if nargin < 4
+    tolerancia = 0.1; % tolerancia en el error de entrenamiento
+end
+if nargin < 5
+    iterMax = 100; % tolerancia en el error de entrenamiento
+end
 
 N = size(X,1);
 nCarac = size(X,2);
 
-w = 0.5*rand(1,nCarac);
+Xbias = [ones(N,1), X];
+y = zeros(size(yd));
 
-for i = 1:nCarac
+w = 0.5*rand(1,nCarac+1); % inicializo los pesos, incluido el del bias
+error = 1;
+iter = 0;
 
+while (error > tolerancia) && (iter < iterMax)
+    iter = iter + 1;
+    for n = 1:N
+        y(n) = sign( dot( w, Xbias(n,:) ) );% salida del perceptron para el ejemplo X(j)
+        w = w + alfa * (yd(n) - y(n)) * Xbias(n,:); % ajuste de w, si d ~= y
+    end
+    error = norm(yd-y) / N;
+end
+iter
+
+%Encontrado w actualizo las clases predichas
+for n = 1:N
+    y(n) = sign( dot( w, Xbias(n,:) ) );% salida del perceptron para el ejemplo X(j)
 end
 
 end
