4eb7bbaa7fbd15c49685c0dc30740bcb3ea69c75
srvanrell <srvanrell@gmail.com>
2014-12-16 12:34:53 -0300

    Corregido problema svm

--- Ejercicio9.m
+++ Ejercicio9.m
@@ -1,12 +1,9 @@
-% COMPLETAR DESCRIPCION RESUMIDA DE LO QUE HACE LA FUNCION QUADPROG
-%
-% x = quadprog(H,f) returns a vector x that minimizes 1/2*x'*H*x + f'*x. H  must be positive definite for the problem to have a finite minimum.
+% x = quadprog(H,f,A,b,Aeq,beq) resuelve el siguiente problema:
 % 
-% x = quadprog(H,f,A,b) minimizes 1/2*x'*H*x + f'*x subject to the restrictions A*x ≤ b. A is a matrix of doubles, and b is a vector of doubles.
-% 
-% x = quadprog(H,f,A,b,Aeq,beq) solves the preceding problem subject to the additional restrictions Aeq*x = beq. Aeq is a matrix of doubles, and beq is a vector of doubles. If no inequalities exist, set A = [] and b = [].
-% 
-% x = quadprog(H,f,A,b,Aeq,beq,lb,ub) solves the preceding problem subject to the additional restrictions lb ≤ x ≤ ub. lb and ub are vectors of doubles, and the restrictions hold for each x component. If no equalities exist, set Aeq = [] and beq = [].
+%  min 1/2*x'*H*x + f'*x 
+%  
+%  sujeto a:   A*x ≤ b
+%            Aeq*x = beq             
 
 %% 9.1 Obtiene los alfas del problema linealmente separable
 
@@ -42,7 +39,7 @@ f = (-1).* [1 1 1]; % (-1) para pasar de maximizacion a minimizacion
 C = 1000000;
 A = [(-1).*eye(3); eye(3)]; 
 b = [0; 0; 0; C ; C; C];
-Aeq = [1 -1 -1];
+Aeq = [1 -1 1];
 beq = 0;
 
 % Se obtienen los alfas
